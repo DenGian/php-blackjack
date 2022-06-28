47bbf35446ede66462378b35b6d9e92209c614ec
DenGian <mondelaers.ian@gmail.com>
2022-06-28 11:32:36 +0200

    added buttons

--- index.php
+++ index.php
@@ -29,10 +29,12 @@ foreach ($deck->getCards() as $card) {
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Blackjack</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-<button></button>
-<button></button>
-<button></button>
+<button type="button" class="btn btn-success" name="hit" formmethod="post">Hit</button>
+<button type="button" class="btn btn-warning" name="stand" formmethod="post">Stand</button>
+<button type="button" class="btn btn-danger" name="surrender" formmethod="post">Surrender</button>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
